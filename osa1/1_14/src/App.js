b3799a64efcb6b7a5e510668e79d29afe1cdb6e2
EeegorL <egorfinn@gmail.com>
2023-05-20 20:18:43 +0300

    osat 1_12-1_13, sekä 1_14 aloitusta

---
+++ osa1/1_14/src/App.js
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+const App = () => {
+  const [selected, setSelected] = useState(0);
+  const [ratings, setRatings] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
+  const [mostVoted, setMostVoted] = useState(0);
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+
+  const nextAnecdote = () => {
+    let next = selected == anecdotes.length - 1 ? 0 : selected + 1;
+    setSelected(next);
+  }
+
+  const voteForAnecdote = () => {
+    let ratingsCopy = ratings.slice(); //slice aiheuttaa react-componentin uudelleen renderöitymisen. ei ole hienoin tapa mutta toimii :P
+    ratingsCopy[selected]++;
+    setRatings(ratingsCopy);
+    findMostVoted();
+  }
+
+  let selectedAnecdote = anecdotes[selected];
+
+  const findMostVoted = () => {
+
+    var largest = ratings[0];
+
+    for (var i = 0; i < ratings.length; i++) {
+      if (largest < ratings[i]) {
+        largest = ratings[i];
+      }
+    }
+  }
+
+  return (
+    <div>
+      <Data selected={selectedAnecdote} votes={ratings[selected]} />
+      <Buttons next={nextAnecdote} vote={voteForAnecdote} />
+      <Favorite />
+    </div>
+  )
+}
+
+const Data = ({ selected, votes }) => {
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{selected}</p>
+      <p>Voting: {votes}</p>
+    </div>
+  )
+}
+
+const Buttons = ({ next, vote }) => {
+  return (
+    <div>
+      <button onClick={() => next()}>Seuraava</button>
+      <button onClick={() => vote()}>+1</button>
+    </div>
+  )
+}
+
+const Favorite = ({ mostVoted }) => {
+  return (
+    <div>
+      <h1>Suosikkijekku</h1>
+      <p>{mostVoted}</p>
+    </div>
+  )
+}
+
+
+export default App
