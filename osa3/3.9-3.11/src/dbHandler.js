67d8f27a72d9864a1236137e004f6fc7dd3ef725
EeegorL <egorfinn@gmail.com>
2023-08-22 22:15:47 +0300

    aaaa se oli tästä

--- osa3/3.9-3.11/src/dbHandler.js
+++ osa3/3.9-3.11/src/dbHandler.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/api/person';
+const baseUrl = 'https://fullstack-3-9-11.onrender.com/';
 
 const doGetAll = async () => {
   const request = await axios.get(baseUrl).catch(err => console.log(err));
