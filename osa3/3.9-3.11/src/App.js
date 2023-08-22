5dfb6b8f5da207afbd259dfc6da6905f5a76c478
EeegorL <egorfinn@gmail.com>
2023-08-22 22:51:20 +0300

    s<gsd<g

--- osa3/3.9-3.11/src/App.js
+++ osa3/3.9-3.11/src/App.js
@@ -90,7 +90,7 @@ const Henkilot = ({ persons, filter, setPersons, setStatus }) => {
   if(persons.length === 0) persons = [{id:1, name:"abc", number:123}, {id:2, name:"def", number: 456}];
   console.log(persons);
   console.log("ererheh")
-  return persons.map(person => {
+  return persons?.map(person => {
     if (person.name.toLowerCase().includes(filter.toLowerCase())) {
       return (
         <div key={person.id}>
